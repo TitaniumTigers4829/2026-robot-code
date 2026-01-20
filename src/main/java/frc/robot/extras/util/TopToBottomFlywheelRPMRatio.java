643da4611379d5e8c96cd11026cd27952e5eaf37
aridavidson001 <aridavidson6@gmail.com>
2026-01-20 15:56:22 -0500

    jst trust me on this one

---
+++ src/main/java/frc/robot/extras/util/TopToBottomFlywheelRPMRatio.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.extras.util;
+
+/** Add your docs here. */
+public class TopToBottomFlywheelRPMRatio {
+    //Okay here's the plan: lookup table if above 45 degs then bottom spins faster on a ratio i'll figure out
+    //later if less than 45 degrees then top spins faster on the same ratio
+    //then figure out velocity needed with that physics equation i did on another paper
+    //velocity i believe is the average velocity of the top and bottom flywheel
+}
--- src/main/java/frc/robot/subsystems/shooter/PhysicalShooter.java
+++ src/main/java/frc/robot/subsystems/shooter/PhysicalShooter.java
@@ -4,7 +4,32 @@
 
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.subsystems.shooter.ShooterInterface;
 
 /** Add your docs here. */
-public class PhysicalShooter implements ShooterInterface {}
+public class PhysicalShooter implements ShooterInterface {
+    private final TalonFX topFlywheelMotor = new TalonFX(ShooterConstants.TOP_FLYWHEEL_MOTOR_ID);
+  private final TalonFX bottomFlywheelMotor = new TalonFX(ShooterConstants.BOTTOM_FLYWHEEL_MOTOR_ID);
+
+  private final MotionMagicVoltage mmPositionRequest = new MotionMagicVoltage(0.0);
+  private final DutyCycleOut dutyCyleOut = new DutyCycleOut(0.0);
+
+  private final MotionMagicTorqueCurrentFOC mmTorqueRequest = new MotionMagicTorqueCurrentFOC(0.0);
+  private final TorqueCurrentFOC currentOut = new TorqueCurrentFOC(0.0);
+ 
+
+  private final TalonFXConfiguration elevatorConfig = new TalonFXConfiguration();
+}
