f3ef3c9ec54c61a2a906aec47874fbf4400302eb
aridavidson001 <aridavidson6@gmail.com>
2026-02-02 17:38:56 -0500

    field relative
    Co-authored-by: Jack <JacksonElia@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "922e267f2d18e9473c1b88907a1922941811a253";
-  public static final String GIT_DATE = "2026-01-30 19:01:58 EST";
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "2c42deade8191ca2dc736a319a25c7e7dc6d4619";
+  public static final String GIT_DATE = "2026-02-02 13:05:00 EST";
   public static final String GIT_BRANCH = "swerve-drive";
-  public static final String BUILD_DATE = "2026-02-02 13:00:59 EST";
-  public static final long BUILD_UNIX_TIME = 1770055259604L;
+  public static final String BUILD_DATE = "2026-02-02 17:24:36 EST";
+  public static final long BUILD_UNIX_TIME = 1770071076509L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
