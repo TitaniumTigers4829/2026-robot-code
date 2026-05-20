f621225d309d97bda36087c1b1ebfd1d037d7286
aridavidson001 <aridavidson6@gmail.com>
2026-05-20 16:53:51 -0400

    ji

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "robot-code-2026-2";
+  public static final String MAVEN_NAME = "robot-code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 166;
-  public static final String GIT_SHA = "a2dec3256ebfc073edf4c410928660d6e5b1fd2f";
-  public static final String GIT_DATE = "2026-04-12 10:23:10 EDT";
+  public static final int GIT_REVISION = 167;
+  public static final String GIT_SHA = "b06b543c713101689abcb17da58d2385b8eaeea9";
+  public static final String GIT_DATE = "2026-04-20 16:23:35 EDT";
   public static final String GIT_BRANCH = "elon-plus-shoot-while-move";
-  public static final String BUILD_DATE = "2026-04-15 19:13:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1776294783480L;
+  public static final String BUILD_DATE = "2026-05-20 16:30:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1779309041880L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
